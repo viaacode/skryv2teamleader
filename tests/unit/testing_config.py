785d673ab6c27d3587c0da8f474cb9df63a26e6a
Walter Schreppers <walter@sitweb.eu>
2022-05-24 13:46:17 +0200

    remove zendesk config data, not needed here.

--- tests/unit/testing_config.py
+++ tests/unit/testing_config.py
@@ -4,11 +4,6 @@ def tst_app_config():
             'token': 'test_token',
             'channel': 'test_channel',
         },
-        'zendesk': {
-            'email': 'test_email@example.com',
-            'token': 'test_zendesk_token',
-            'subdomain': 'test_zendesk_domain'
-        },
         'environment': 'TST',
         'teamleader': {
             'webhook_url': 'http://testing_webhook_url'
