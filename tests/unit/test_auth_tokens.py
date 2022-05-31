2b8ddf639f1b8a5655951d932e9f0f97bbb39483
Walter Schreppers <walter@sitweb.eu>
2022-05-31 17:36:01 +0200

    add auth token tests

---
+++ tests/unit/test_auth_tokens.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  @Author: Walter Schreppers
+#
+#   tests/unit/test_auth_tokens.py
+#
+
+from app.clients.teamleader_auth import TeamleaderAuth
+
+
+class TestTeamleaderAuth:
+    def test_token_saving(self):
+        ta = TeamleaderAuth({'token_file': 'test_tokens.pkl'})
+        ta.reset()
+        assert ta.tokens_available() is False
+
+        ta.save('somecode', 'some_auth', 'some_refresh')
+        assert ta.tokens_available()
+        assert ta.read() == ('somecode', 'some_auth', 'some_refresh')
+
+        ta.reset()
+        assert ta.tokens_available() is False
