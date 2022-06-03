8a58273847163118083de56e69aa2a7e87a98ac3
Walter Schreppers <walter@sitweb.eu>
2022-06-03 10:42:30 +0200

    custom fields added for tests. mapping now done with uuid's instead of labels.

--- app/services/document_service.py
+++ app/services/document_service.py
@@ -20,8 +20,19 @@ class DocumentService:
         config = ConfigParser()
         self.skryv_config = config.app_cfg['skryv']
         self.SKRYV_DOSSIER_CP_ID = self.skryv_config['dossier_content_partner_id']
-        self.custom_fields = config.app_cfg['custom_field_labels']
-        print("custom fields=", self.custom_fields)
+        self.custom_fields = self.custom_field_mapping(config.app_cfg['custom_field_ids'])
+
+    def custom_field_mapping(self, field_ids):
+        self.custom_fields = {}
+        for f in self.tlc.list_custom_fields():
+            for f_label, f_id in field_ids.items():
+                if f['id'] == f_id:
+                    self.custom_fields[f_label] = f
+                    # helpful print of mapping for debugging
+                    print(f"custom_fields[{f_label}]={f}")
+
+        return self.custom_fields
+
 
     def postadres(self):
         return self.document.document.value['adres_en_contactgegevens']['postadres']
