dd9a5b919a156eb736f6d238b9e45c0e679030d5
Walter Schreppers <walter@sitweb.eu>
2022-06-27 13:17:42 +0200

    start testing of methods in teamleader client class using requests_mock

--- tests/unit/testing_config.py
+++ tests/unit/testing_config.py
@@ -6,9 +6,19 @@ def tst_app_config():
         },
         'environment': 'TST',
         'teamleader': {
+            'auth_uri': 'https://app.teamleader.eu',
+            'api_uri': 'https://api.teamleader.eu',
+            'client_id': 'test_client_id',
+            'client_secret': 'test_client_secret',
+            'secret_code_state': 'test_secret_code_state',
+            'code': 'test_teamleader_code',
+            'auth_token': 'test_teamleader_auth_token',
+            'refresh_token': 'test_teamleader_refresh_token',
+            'redirect_uri': 'https://services-qas.testing.com',
             'redis': 'redis://localhost:6543'
         },
         'skryv': {
-            'webhook_url': 'http://localhost:8080'
+            'webhook_url': 'http://localhost:8080',
+            'webhook_jwt': 'some_jwt_here_to_secure_route'
         }
     }
